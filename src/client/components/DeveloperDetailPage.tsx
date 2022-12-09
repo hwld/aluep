ffce31552d4945ca60a3e1388f17594185b6e50c
unknown <unknown>
2022-12-09 16:29:51 +0900

    開発者詳細画面　更新

--- src/client/components/DeveloperDetailPage.tsx
+++ src/client/components/DeveloperDetailPage.tsx
@@ -1,86 +1,25 @@
 import {
   ActionIcon,
   Avatar,
-  Button,
+  Box,
   Card,
   Flex,
   Textarea,
+  Tooltip,
   useMantineTheme,
 } from "@mantine/core";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React from "react";
 import { BsGithub } from "react-icons/bs";
 import { MdOutlineFavorite } from "react-icons/md";
 import { Theme } from "../../server/models/theme";
 import { ThemeDeveloper } from "../../server/models/themeDeveloper";
-import { useAllThemesQuery } from "../hooks/useAllThemesQuery";
-import { useThemeDevelopersQuery } from "../hooks/useThemeDevelopersQuery";
-import { useThemeLike } from "../hooks/useThemeLike";
 import { ThemeCard } from "./ThemeCard/ThemeCard";
 
 type Props = { developer: ThemeDeveloper; theme: Theme };
 
 export const DeveloperDetailPage: React.FC<Props> = ({ developer, theme }) => {
-  const router = useRouter();
-  const themeId = router.query.id as string;
-
-  const { allThemes } = useAllThemesQuery();
-
-  const { likeThemeMutation, likedByLoggedInUser } = useThemeLike(themeId);
-
-  const { developers, likeDeveloperMutation } =
-    useThemeDevelopersQuery(themeId);
-
-  const handleLikeTheme = () => {
-    if (!theme) return;
-    likeThemeMutation.mutate({
-      themeId: theme?.id,
-      like: !likedByLoggedInUser,
-    });
-  };
-
   const mantineTheme = useMantineTheme();
-
-  const [isPostTheme, setIsPostTheme] = useState(true);
-  const [isJoinTheme, setIsJoinTheme] = useState(false);
-  const [isLike, setIsLike] = useState(false);
-
-  const handlePostTheme = () => {
-    if (!isPostTheme) {
-      setIsPostTheme(!isPostTheme);
-    }
-    if (isJoinTheme) {
-      setIsJoinTheme(false);
-    }
-    if (isLike) {
-      setIsLike(false);
-    }
-  };
-
-  const handleJoinTheme = () => {
-    if (!isJoinTheme) {
-      setIsJoinTheme(!isJoinTheme);
-    }
-    if (isPostTheme) {
-      setIsPostTheme(false);
-    }
-    if (isLike) {
-      setIsLike(false);
-    }
-  };
-
-  const handleLike = () => {
-    if (!isLike) {
-      setIsLike(!isLike);
-    }
-    if (isPostTheme) {
-      setIsPostTheme(false);
-    }
-    if (isJoinTheme) {
-      setIsJoinTheme(false);
-    }
-  };
 
   return (
     <Flex maw={1200} direction="column" align="center" m="auto">
@@ -104,117 +43,83 @@ export const DeveloperDetailPage: React.FC<Props> = ({ developer, theme }) => {
 
           <Flex gap={70} mt={10} wrap="wrap" justify={"center"}>
             <Flex align={"center"} gap={15} wrap="wrap" direction={"column"}>
-              <MdOutlineFavorite size="30" style={{ marginTop: "4px" }} />
-
-              {/* {themes.map((theme) => {
-              if (theme.user.id === session?.user.id) {
-                return <ThemeCard key={theme.id} theme={theme} />;
-              }
-            })} */}
+              <Box>
+                <Tooltip
+                  label="開発者へのいいねの合計"
+                  color="gray.5"
+                  position="top"
+                  withArrow
+                  transition="pop"
+                >
+                  <Flex align={"center"} wrap="wrap" direction={"column"}>
+                    <MdOutlineFavorite size="30" style={{ marginTop: "2px" }} />
+                  </Flex>
+                </Tooltip>
+              </Box>
+              <div style={{ marginTop: "-12px" }}>{developer.likes}</div>
             </Flex>
             <Flex align={"center"} gap={15} wrap="wrap" direction={"column"}>
-              <ActionIcon
-                size={30}
-                component={Link}
-                href={developer.githubUrl}
-                target="_blank"
-                sx={(theme) => ({
-                  transition: "all 200ms",
-                  "&:hover": {
-                    backgroundColor: theme.fn.rgba(theme.colors.gray[7], 0.1),
-                  },
-                })}
-              >
-                <BsGithub size="80%" fill={mantineTheme.colors.gray[7]} />
-              </ActionIcon>
-
-              <div>GitHub</div>
+              <Box>
+                <Tooltip
+                  label="GitHubへのアクセス"
+                  color="gray.5"
+                  position="top"
+                  withArrow
+                  transition="pop"
+                >
+                  <Flex align={"center"} wrap="wrap" direction={"column"}>
+                    <ActionIcon
+                      size={30}
+                      component={Link}
+                      href={developer.githubUrl}
+                      target="_blank"
+                      sx={(theme) => ({
+                        transition: "all 200ms",
+                        "&:hover": {
+                          backgroundColor: theme.fn.rgba(
+                            theme.colors.gray[7],
+                            0.1
+                          ),
+                        },
+                      })}
+                    >
+                      <BsGithub size="100" fill={mantineTheme.colors.gray[7]} />
+                    </ActionIcon>
+                  </Flex>
+                </Tooltip>
+                <div style={{ marginTop: "4px" }}>GitHub</div>
+              </Box>
             </Flex>
           </Flex>
         </Card>
 
         <Card mih={20} w={500}>
           <Card.Section withBorder inheritPadding py="md">
-            <div>自己紹介</div>
+            <div>コメント</div>
           </Card.Section>
 
           <Card.Section inheritPadding mt="sm" pb="md">
-            <Textarea placeholder="自己紹介..." withAsterisk readOnly />
+            <Textarea
+              placeholder="開発に対するコメント"
+              value={developer.comment}
+              withAsterisk
+              readOnly
+              autosize
+            />
           </Card.Section>
         </Card>
       </Flex>
-      <Flex mt={30}>
-        <Button.Group>
-          <Button
-            variant="light"
-            w={130}
-            onClick={handlePostTheme}
-            bg={isPostTheme ? "gray.0" : "gray.3"}
-            color="dark"
-            sx={(theme) => {
-              return { "&:hover": { backgroundColor: theme.colors.gray[4] } };
-            }}
-          >
-            投稿したお題
-          </Button>
-          <Button
-            variant="light"
-            w={130}
-            onClick={handleJoinTheme}
-            bg={isJoinTheme ? "gray.0" : "gray.3"}
-            color="dark"
-            sx={(theme) => {
-              return { "&:hover": { backgroundColor: theme.colors.gray[4] } };
-            }}
-          >
-            参加したお題
-          </Button>
-          <Button
-            variant="light"
-            w={130}
-            onClick={handleLike}
-            bg={isLike ? "gray.0" : "gray.3"}
-            color="dark"
-            sx={(theme) => {
-              return { "&:hover": { backgroundColor: theme.colors.gray[4] } };
-            }}
-          >
-            いいね
-          </Button>
-        </Button.Group>
+      <Flex mt={45} w={760}>
+        <ThemeCard key={theme.id} theme={theme} />
       </Flex>
-      {isPostTheme && (
-        <div>
-          <Flex mt={30} gap={15} wrap="wrap" direction={"column"}>
-            投稿したお題を表示
-            {theme.user.id}_____
-            {developer.id}
-            {allThemes?.map((theme) => {
-              if (theme.user.id === developer.id) {
-                return <ThemeCard key={theme.id} theme={theme} />;
-              }
-            })}
-          </Flex>
-        </div>
-      )}
-      {isJoinTheme && (
+
+      {/* {isJoinTheme && (
         <div>
           <Flex mt={30} gap={15} wrap="wrap">
-            {allThemes?.map((theme) => {
-              if (theme.id === developer.themeId) {
-                return <ThemeCard key={theme.id} theme={theme} />;
-              }
-            })}
+            <ThemeCard key={theme.id} theme={theme} />
           </Flex>
         </div>
-      )}
-      {isLike && (
-        <div>
-          <Flex mt={30} gap={15} wrap="wrap">
-            いいねを表示
-          </Flex>
-        </div>
-      )}
+      )} */}
     </Flex>
   );
 };
