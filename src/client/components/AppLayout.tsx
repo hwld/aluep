534d2abe38790d8a8eb37f5551fbabc807f9190e
hwld <0701hw@gmail.com>
2023-01-12 15:15:50 +0900

    add: 画面幅が小さい場合にメッセージを表示させる

--- src/client/components/AppLayout.tsx
+++ src/client/components/AppLayout.tsx
@@ -1,14 +1,18 @@
 import { Box } from "@mantine/core";
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
 import { ReactNode } from "react";
 import { useSessionQuery } from "../hooks/useSessionQuery";
 import { AppHeader } from "./AppHeader/AppHeader";
+import { AppModal } from "./AppModal";
 import { RequireLoginModal } from "./RequireLoginModal";
 
 type Props = { children: ReactNode };
 
 export const AppLayout: React.FC<Props> = ({ children }) => {
   const { session } = useSessionQuery();
-
+  // TODO: スマートフォンなどの幅の狭い端末に対応していない
+  const unsupportedDisplay = useMediaQuery("(max-width: 800px)");
+  const [isOpen, { close }] = useDisclosure(true);
   return (
     <>
       <Box
@@ -31,8 +35,15 @@ export const AppLayout: React.FC<Props> = ({ children }) => {
         >
           {children}
         </Box>
+        <RequireLoginModal />
       </Box>
-      <RequireLoginModal />
+      {/* TODO: まだスマートフォンなどに対応していないため、初訪問時に知らせる。 */}
+      {unsupportedDisplay && (
+        <AppModal title="お知らせ" opened={isOpen} onClose={close}>
+          まだ対応していない端末のため、画面のレイアウトに問題が発生する可能性があります。
+          PCやタブレットなどの幅の広い端末を使用してください。
+        </AppModal>
+      )}
     </>
   );
 };
