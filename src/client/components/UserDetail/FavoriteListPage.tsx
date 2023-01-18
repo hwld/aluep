e0ca74a872d9ceb9f0a330c6a68d21a9c3d10405
unknown <unknown>
2023-01-18 09:35:00 +0900

    開発者をお気に入りしていないときの画面の作成

--- src/client/components/UserDetail/FavoriteListPage.tsx
+++ src/client/components/UserDetail/FavoriteListPage.tsx
@@ -1,7 +1,9 @@
-import { Box, Card, Flex, Stack, Text } from "@mantine/core";
+import { Box, Card, Flex, Stack, Text, useMantineTheme } from "@mantine/core";
 import { User } from "@prisma/client";
 
 import React from "react";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { MdFavorite } from "react-icons/md";
 import { useFavoriteListQuery } from "../../hooks/useFavoriteListQuery";
 import { usePaginationState } from "../../hooks/usePaginationState";
 import { AppPagination } from "../AppPagination";
@@ -12,6 +14,7 @@ type Props = { user: User };
 export const FavoriteListPage: React.FC<Props> = ({ user }) => {
   const [favoPage, setFavoPage] = usePaginationState({});
   const { favoriteList } = useFavoriteListQuery(user.id, favoPage);
+  const mantineTheme = useMantineTheme();
 
   return (
     <Flex
@@ -24,7 +27,7 @@ export const FavoriteListPage: React.FC<Props> = ({ user }) => {
       })}
     >
       <Card>
-        <Text align="center" size="xl" fw={700}>
+        <Text align="center" size={40} fw={700}>
           {user.name}
         </Text>
         <hr />
@@ -34,17 +37,39 @@ export const FavoriteListPage: React.FC<Props> = ({ user }) => {
       </Card>
 
       <Stack mt="md" w="100%">
-        <Box
-          sx={(theme) => ({
-            display: "grid",
-            gridTemplateColumns: `repeat(auto-fit, minmax(${userCardMinWidthPx}px, 1fr))`,
-            gap: theme.spacing.md,
-          })}
-        >
-          {favoriteList?.pagefavo.map((user) => {
-            return <UserCard key={user.id} user={user} />;
-          })}
-        </Box>
+        {favoriteList?.pagefavo.length === 0 ? (
+          <Flex direction={"column"} align={"center"} gap={10}>
+            <Card bg={"red.1"}>
+              <Flex align={"flex-end"}>
+                <BsFillPeopleFill
+                  size={100}
+                  color={mantineTheme.colors.red[7]}
+                />
+                <MdFavorite size={100} color={mantineTheme.colors.red[3]} />
+                <MdFavorite size={130} color={mantineTheme.colors.red[4]} />
+                <MdFavorite size={160} color={mantineTheme.colors.red[5]} />
+                <MdFavorite size={190} color={mantineTheme.colors.red[6]} />
+              </Flex>
+            </Card>
+            <Text size={30}>開発者をお気に入りしてみよう！</Text>
+
+            <Text c="gray.5">
+              他の開発者をお気に入りすると、ここに表示されます。
+            </Text>
+          </Flex>
+        ) : (
+          <Box
+            sx={(theme) => ({
+              display: "grid",
+              gridTemplateColumns: `repeat(auto-fit, minmax(${userCardMinWidthPx}px, 1fr))`,
+              gap: theme.spacing.md,
+            })}
+          >
+            {favoriteList?.pagefavo.map((user) => {
+              return <UserCard key={user.id} user={user} />;
+            })}
+          </Box>
+        )}
 
         <AppPagination
           page={Number(favoPage)}
