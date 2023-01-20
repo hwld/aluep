9fad27e0a15a8fbed729a9f626eaefa64ee96451
hwld <0701hw@gmail.com>
2023-01-20 16:14:55 +0900

    add: お題のコメントに返信する機能を実装

--- src/client/components/ThemeComments.tsx
+++ src/client/components/ThemeComments.tsx
@@ -6,7 +6,7 @@ import { useRequireLoginModal } from "../contexts/RequireLoginModalProvider";
 import { useCyclicRandom } from "../hooks/useCyclicRandom";
 import { useSessionQuery } from "../hooks/useSessionQuery";
 import { useThemeComments } from "../hooks/useThemeComments";
-import { CommentCard } from "./CommentCard";
+import { ThemeCommentCard } from "./ThemeCommentCard";
 import { ThemeCommentForm } from "./ThemeCommentForm";
 
 type Props = { themeId: string };
@@ -15,6 +15,8 @@ type Props = { themeId: string };
 export const ThemeComments: React.FC<Props> = ({ themeId }) => {
   const { session } = useSessionQuery();
   const { openLoginModal } = useRequireLoginModal();
+
+  // お題のコメント操作
   const { themeComments, postCommentMutation, deleteCommentMutation } =
     useThemeComments(themeId);
 
@@ -35,13 +37,35 @@ export const ThemeComments: React.FC<Props> = ({ themeId }) => {
   ) => {
     postCommentMutation.mutate(data, {
       onSuccess: () => {
+        // Formのkeyを変更して再マウントさせる。
+        // これでFormのフィールドがリセットされ、submitボタンを一度も押していないことになる。
+        // handleSubmitCommentでフィールドをリセットするメソッドを受けとり、onSuccessで実行することも
+        // できるが、submitを一度押していることになるので、コメント欄に入力->入力をすべて削除でエラーメッセージが表示されてしまう
         nextFormKey();
       },
     });
   };
 
+  const handleSubmitReply = (
+    data: OmitStrict<ThemeCommentFormData, "themeId">,
+    onSuccess: () => void
+  ) => {
+    postCommentMutation.mutate(data, { onSuccess });
+  };
+
   const handleDeleteComment = (commentId: string) => {
     deleteCommentMutation.mutate(commentId);
+  };
+
+  // TODO: サーバー側でThemeCommentに含めたほうが良い？
+  // commentIdからユーザー名を取得する
+  const getUserNameByCommentId = (
+    commentId: string | null | undefined
+  ): string => {
+    return (
+      themeComments?.find((comment) => comment.id === commentId)?.fromUser
+        .name ?? "不明なユーザー名"
+    );
   };
 
   return (
@@ -52,9 +76,13 @@ export const ThemeComments: React.FC<Props> = ({ themeId }) => {
       <Stack spacing="xs">
         {themeComments?.map((comment) => {
           return (
-            <CommentCard
+            <ThemeCommentCard
               key={comment.id}
               comment={comment}
+              inReplyToUserName={getUserNameByCommentId(
+                comment.inReplyToCommentId
+              )}
+              onReplyComment={handleSubmitReply}
               onDeleteComment={handleDeleteComment}
               isDeleting={deleteCommentMutation.isLoading}
               loggedInUserId={session?.user.id}
