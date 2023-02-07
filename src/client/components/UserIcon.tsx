c963e24a970644700bad27dc16212d95a0308964
hwld <0701hw@gmail.com>
2023-02-07 11:07:30 +0900

    add: ユーザーアイコンを読み込めないときのフォールバック処理を追加

--- src/client/components/UserIcon.tsx
+++ src/client/components/UserIcon.tsx
@@ -1,5 +1,5 @@
 import { Avatar, AvatarProps } from "@mantine/core";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 export type UserIconProps = {
   iconSrc?: string | null;
@@ -13,26 +13,53 @@ export const UserIcon: React.FC<UserIconProps> = ({
   size,
   withBorder = true,
 }) => {
+  const iconRef = useRef<HTMLImageElement | null>(null);
+
+  useEffect(() => {
+    if (typeof iconSrc !== "string") {
+      return;
+    }
+
+    const image = new Image();
+    image.src = iconSrc;
+
+    // SSRを使用していると、imgのonErrorが補足されない (https://github.com/facebook/react/issues/15446)
+    // そのため、iconSrcが変わるたびにimg要素を作成して、読み込みを試す
+    // ただ、UserIconが使われるたびに画像を読み込むので、リクエストが多くなってしまう。
+    // GCSのキャッシュを工夫する必要がある？
+    image.onerror = () => {
+      if (!iconRef.current) {
+        return;
+      }
+
+      // mantineのAvatarはimgをBoxでラップしてるので、img子要素をみつけてエラーイベントを手動で投げる
+      iconRef.current.querySelector("img")?.dispatchEvent(new Event("error"));
+    };
+  }, [iconSrc]);
+
   return (
-    <Avatar
-      // スタイルを当てるのにも使用している
-      aria-label="user-icon"
-      src={iconSrc}
-      size={size}
-      sx={(theme) => ({
-        borderRadius: "100%",
-        transition: "all 150ms",
-        ...(withBorder
-          ? {
-              borderWidth: "2px",
-              borderColor: theme.colors.gray[2],
-              borderStyle: "solid",
-            }
-          : {}),
-        ...(interactive
-          ? { "&:hover": { borderColor: theme.colors.red[8] } }
-          : {}),
-      })}
-    />
+    <>
+      <Avatar
+        ref={iconRef}
+        // スタイルを当てるのにも使用している
+        aria-label="user-icon"
+        src={iconSrc}
+        size={size}
+        sx={(theme) => ({
+          borderRadius: "100%",
+          transition: "all 150ms",
+          ...(withBorder
+            ? {
+                borderWidth: "2px",
+                borderColor: theme.colors.gray[2],
+                borderStyle: "solid",
+              }
+            : {}),
+          ...(interactive
+            ? { "&:hover": { borderColor: theme.colors.red[8] } }
+            : {}),
+        })}
+      />
+    </>
   );
 };
