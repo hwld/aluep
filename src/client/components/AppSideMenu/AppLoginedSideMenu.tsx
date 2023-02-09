96918e7d1fca8def06c6b7684ec1576aaec7b859
hwld <0701hw@gmail.com>
2023-02-09 11:48:39 +0900

    change: サイドバーのUIを変更

--- src/client/components/AppSideMenu/AppLoginedSideMenu.tsx
+++ src/client/components/AppSideMenu/AppLoginedSideMenu.tsx
@@ -1,6 +1,12 @@
-import { Flex, Stack, Text, useMantineTheme } from "@mantine/core";
+import {
+  Flex,
+  Stack,
+  Text,
+  UnstyledButton,
+  useMantineTheme,
+} from "@mantine/core";
 import { Session } from "next-auth";
-import { TbCode, TbFileText, TbHeart } from "react-icons/tb";
+import { TbChevronDown, TbCode, TbFileText, TbHeart } from "react-icons/tb";
 import { useLoggedInUserInfoQuery } from "../../hooks/useLoggedInUserInfoQuery";
 import { UserIcon } from "../UserIcon";
 
@@ -16,81 +22,103 @@ export const AppLoginedSideMenu: React.FC<Props> = ({ user, iconWidth }) => {
     <Flex
       w="100%"
       gap="xs"
+      align="center"
+      justify="space-between"
       sx={() => ({
         overflow: "hidden",
       })}
     >
-      <Stack sx={() => ({ flexShrink: 0 })}>
-        <UserIcon size={iconWidth} iconSrc={user.image} withBorder={true} />
-      </Stack>
-      <Stack
-        sx={() => ({
-          flexShrink: 1,
-          flexWrap: "nowrap",
-          overflow: "hidden",
-        })}
-        spacing="xs"
-      >
-        <Text
-          c="gray.1"
-          fw="bold"
-          size="sm"
+      <Flex gap="xs">
+        <Stack sx={() => ({ flexShrink: 0 })}>
+          <UserIcon size={iconWidth} iconSrc={user.image} withBorder={true} />
+        </Stack>
+        <Stack
           sx={() => ({
-            whiteSpace: "nowrap",
+            flexShrink: 1,
+            flexWrap: "nowrap",
             overflow: "hidden",
-            textOverflow: "ellipsis",
           })}
+          spacing="xs"
         >
-          {user.name}
-        </Text>
-        {/* アイコン類 */}
-        <Flex gap="xs">
-          <Flex gap="2px" align="center">
-            <TbHeart size="20" color={colors.gray[1]} />
-            <Text
-              size="xs"
-              color={colors.gray[1]}
-              sx={() => ({
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              })}
-            >
-              {loggedInUserInfo?.allLikes ?? 0}
-            </Text>
-          </Flex>
+          <Text
+            c="gray.1"
+            fw="bold"
+            size="sm"
+            sx={() => ({
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            })}
+          >
+            {user.name}
+          </Text>
+          {/* アイコン類 */}
+          <Flex gap="xs">
+            <Flex gap="2px" align="center">
+              <TbHeart size="20" color={colors.gray[1]} />
+              <Text
+                size="xs"
+                color={colors.gray[1]}
+                sx={() => ({
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                })}
+              >
+                {loggedInUserInfo?.allLikes ?? 0}
+              </Text>
+            </Flex>
 
-          <Flex gap="2px" align="center">
-            <TbFileText size="20" color={colors.gray[1]} />
-            <Text
-              size="xs"
-              color={colors.gray[1]}
-              sx={() => ({
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              })}
-            >
-              {loggedInUserInfo?.themes ?? 0}
-            </Text>
-          </Flex>
+            <Flex gap="2px" align="center">
+              <TbFileText size="20" color={colors.gray[1]} />
+              <Text
+                size="xs"
+                color={colors.gray[1]}
+                sx={() => ({
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                })}
+              >
+                {loggedInUserInfo?.themes ?? 0}
+              </Text>
+            </Flex>
 
-          <Flex gap="2px" align="center">
-            <TbCode size="20" color={colors.gray[1]} />
-            <Text
-              size="xs"
-              color={colors.gray[1]}
-              sx={() => ({
-                whiteSpace: "nowrap",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-              })}
-            >
-              {loggedInUserInfo?.developers ?? 0}
-            </Text>
+            <Flex gap="2px" align="center">
+              <TbCode size="20" color={colors.gray[1]} />
+              <Text
+                size="xs"
+                color={colors.gray[1]}
+                sx={() => ({
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                })}
+              >
+                {loggedInUserInfo?.developers ?? 0}
+              </Text>
+            </Flex>
           </Flex>
-        </Flex>
-      </Stack>
+        </Stack>
+      </Flex>
+      <UnstyledButton
+        sx={(theme) => ({
+          borderRadius: "50%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          transition: "background-color 150ms",
+          "&:hover": {
+            backgroundColor: theme.fn.rgba("#000", 0.1),
+          },
+        })}
+      >
+        <TbChevronDown
+          size="45"
+          color={colors.gray[3]}
+          style={{ paddingTop: "3px" }}
+        />
+      </UnstyledButton>
     </Flex>
   );
 };
