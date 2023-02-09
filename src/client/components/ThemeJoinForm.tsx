c26b2cb674928816aa507cf67962be0bd5250308
hwld <0701hw@gmail.com>
2023-02-09 21:36:21 +0900

    change: ThemeJoinFormのスタイル変更

--- src/client/components/ThemeJoinForm.tsx
+++ src/client/components/ThemeJoinForm.tsx
@@ -1,4 +1,4 @@
-import { Radio, Text } from "@mantine/core";
+import { Box, Radio, Stack, Text } from "@mantine/core";
 import React, { useState } from "react";
 import { RepositoryFormData, ThemeJoinFormData } from "../../share/schema";
 import { OmitStrict } from "../../types/OmitStrict";
@@ -16,6 +16,7 @@ type Props = {
   repository: string;
 };
 export const ThemeJoinForm: React.FC<Props> = ({
+  defaultValues,
   themeId,
   repository,
   onSubmit,
@@ -32,50 +33,64 @@ export const ThemeJoinForm: React.FC<Props> = ({
   };
 
   return (
-    <Radio.Group
-      value={val}
-      name="repository"
-      orientation="vertical"
-      label="開発に使用するGitHubリポジトリを選択する"
-      spacing="sm"
-      withAsterisk
-    >
-      {flg ? (
-        <Radio
-          value="already"
-          label="既存のリポジトリを使用する"
-          onClick={handleClick}
-        />
-      ) : (
-        <Text mt={5} size="sm" color="red">
-          GitHubリポジトリを作成することができなかったため、再ログインを行いました。
-          <br />
-          もう一度「開発する」ボタンを押してください。
-        </Text>
-      )}
+    <Stack>
+      <Radio.Group
+        value={val}
+        spacing="xs"
+        name="repository"
+        orientation="vertical"
+        label="開発に使用するGitHubリポジトリを選択する"
+        withAsterisk
+        styles={(theme) => ({ label: { color: theme.colors.gray[5] } })}
+      >
+        {flg ? (
+          <Radio
+            value="already"
+            label="既存のリポジトリを使用する"
+            onClick={handleClick}
+            styles={(theme) => ({
+              label: { color: theme.colors.gray[7] },
+            })}
+          />
+        ) : (
+          <Text mt={5} size="sm" color="red">
+            GitHubリポジトリを作成することができなかったため、再ログインを行いました。
+            <br />
+            もう一度「開発する」ボタンを押してください。
+          </Text>
+        )}
 
-      <Radio
-        value="new"
-        label="新しいリポジトリを作成する"
-        onClick={handleClick}
-      />
-      {val === "already" && (
-        <ThemeJoinAlreadyForm
-          onSubmit={onSubmit}
-          onCancel={onCancel}
-          themeId={themeId}
-          submitText={submitText}
+        <Radio
+          value="new"
+          label="新しいリポジトリを作成する"
+          onClick={handleClick}
+          styles={(theme) => ({
+            label: { color: theme.colors.gray[7] },
+          })}
         />
+      </Radio.Group>
+      {val === "already" && (
+        <Box>
+          <ThemeJoinAlreadyForm
+            defaultValues={defaultValues}
+            onSubmit={onSubmit}
+            onCancel={onCancel}
+            themeId={themeId}
+            submitText={submitText}
+          />
+        </Box>
       )}
 
       {val === "new" && (
-        <ThemeJoinNewForm
-          onCancel={onCancel}
-          themeId={themeId}
-          submitText={submitText}
-          repoFormData={repoFormData}
-        />
+        <Box>
+          <ThemeJoinNewForm
+            onCancel={onCancel}
+            themeId={themeId}
+            submitText={submitText}
+            repoFormData={repoFormData}
+          />
+        </Box>
       )}
-    </Radio.Group>
+    </Stack>
   );
 };
