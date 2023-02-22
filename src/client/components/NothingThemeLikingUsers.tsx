d2d24c4e140d095aedf89380697f5bf3ec8ec86e
hwld <0701hw@gmail.com>
2023-02-23 01:27:01 +0900

    change: アイコンの変更

--- src/client/components/NothingThemeLikingUsers.tsx
+++ src/client/components/NothingThemeLikingUsers.tsx
@@ -6,7 +6,7 @@ export const NothingThemeLikingUsers = () => {
   return (
     <Flex mt={30} direction={"column"} align={"center"} gap={15}>
       <Flex align={"flex-end"}>
-        <Image src="/logo.svg" alt="logo" width={60} height={60} />
+        <Image src="/logo.svg" alt="logo" width={80} height={80} />
         <TbHeart size={80} color={mantineTheme.colors.red[7]} />
       </Flex>
       <Text fz="xl" align="center">
