0a687e3055f07ae04ba91c9f220b3320087f7d39
hwld <0701hw@gmail.com>
2023-03-06 23:04:12 +0900

    fix

--- src/client/ui/WrapperLink.tsx
+++ src/client/ui/WrapperLink.tsx
@@ -1,11 +1,16 @@
 import Link, { LinkProps } from "next/link";
-import React, { forwardRef, ReactElement, ReactNode } from "react";
+import React, {
+  forwardRef,
+  HTMLAttributeAnchorTarget,
+  ReactElement,
+  ReactNode,
+} from "react";
 
-type Props = { children: ReactNode } & (
+export type WrapperLinkProps = { children?: ReactNode } & (
   | { noWrap: true }
-  | ({ noWrap?: false } & LinkProps)
+  | ({ noWrap?: false; target?: HTMLAttributeAnchorTarget } & LinkProps)
 );
-export const WrapperLink = forwardRef<HTMLElement, Props>(
+export const WrapperLink = forwardRef<HTMLElement, WrapperLinkProps>(
   ({ children, ...props }, ref) => {
     if (props.noWrap) {
       // Tooltip直下で使用すると、様々なpropsが渡されるので、それを直接childrenに渡す
