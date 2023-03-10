63074b673c45fcae31748f616df272be28d64497
hwld <0701hw@gmail.com>
2023-03-10 15:17:35 +0900

    change: Sessionのuserをuserモデルに変更

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,16 +1,13 @@
-import { DefaultSession } from "next-auth";
+import "next-auth";
+import { User } from "../server/models/user";
 
 // DBにあるuserIdをセッションに含めるための型定義
 // api/auth/[...nextauth].ts でcallbackを設定して、userIdをセットしている。
 declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   * TODO:
    */
   interface Session {
-    user: DefaultSession["user"] & {
-      id: string;
-      profile?: string | null;
-    };
+    user: User;
   }
 }
