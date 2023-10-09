6111a544b326667f87118c74a4835587cf2ee1e0
hwld <0701hw@gmail.com>
2023-10-10 08:19:30 +0900

    refactor: zodのcoerceを使用して、InputTypeにstringが含まれないようにした

--- src/share/paging.ts
+++ src/share/paging.ts
@@ -1,18 +1,11 @@
 import { z } from "zod";
 
-// queryとして数字を渡せないのでGSSP用にstring()を追加する
-// URLのパース以外では手動で文字列を渡さないようにする。
-// TODO: coerce使えば入力をnumberに制限しながら、stringが渡ってきたときもNumber(string)でnumberにできそう
-export const pagingSchema = z
-  .string()
-  .or(z.number())
-  .transform((page) => {
-    const p = Number(page);
-    if (isNaN(p) || p < 0) {
-      return 1;
-    }
-    return p;
-  });
+export const pagingSchema = z.coerce.number().transform((page) => {
+  if (isNaN(page) || page < 0) {
+    return 1;
+  }
+  return page;
+});
 
 /** ページングのある画面用のスキーマ */
 export const paginatedPageSchema = z.object({ page: pagingSchema.default(1) });
