0c04462fa4a6532c76467d49b794d20fb0bb8663
hwld <0701hw@gmail.com>
2023-10-12 16:03:15 +0900

    design: uiの変更

--- src/client/features/idea/IdeaDescriptionView/IdeaDescriptionView.tsx
+++ src/client/features/idea/IdeaDescriptionView/IdeaDescriptionView.tsx
@@ -5,6 +5,7 @@ type Props = { descriptionHtml: string };
 export const IdeaDescriptionView: React.FC<Props> = ({ descriptionHtml }) => {
   return (
     <Box
+      style={{ padding: "var(--mantine-spacing-sm) var(--mantine-spacing-md)" }}
       className={editorClasses.content}
       dangerouslySetInnerHTML={{ __html: descriptionHtml }}
     ></Box>
