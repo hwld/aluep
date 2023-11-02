760eeab8af478e2243664cb09b7d200cb969aada
hwld <0701hw@gmail.com>
2023-11-03 05:26:56 +0900

    feat: UIの変更

--- src/client/features/session/RequireLoginModal/RequireLoginModal.tsx
+++ src/client/features/session/RequireLoginModal/RequireLoginModal.tsx
@@ -1,7 +1,7 @@
 import { useRequireLoginModal } from "@/client/features/session/RequireLoginModalProvider";
 import { AppModal } from "@/client/ui/AppModal/AppModal";
-import { Button, Stack, Text } from "@mantine/core";
-import { IconBrandGithub } from "@tabler/icons-react";
+import { Button, Center, Flex, Stack, Text } from "@mantine/core";
+import { IconBrandGithub, IconLogin2 } from "@tabler/icons-react";
 import { signIn } from "next-auth/react";
 
 type Props = {};
@@ -19,16 +19,33 @@ export const RequireLoginModal: React.FC<Props> = () => {
       opened={isLoginModalOpen}
       onClose={closeLoginModal}
       title="ログイン"
+      size="lg"
     >
-      <Stack>
-        <Text>この機能を利用するためには、ログインをする必要があります。</Text>
-        <Button
-          leftSection={<IconBrandGithub width={21} height={21} />}
-          onClick={handleLogin}
+      <Flex gap="xl" pt={0} p="lg">
+        <Stack style={{ flex: 1 }} mt="xl" justify="center" gap="xl">
+          <Text>
+            この機能を利用するためには、GitHubアカウントでログインする必要があります。
+          </Text>
+          <Button
+            leftSection={<IconBrandGithub width={21} height={21} />}
+            onClick={handleLogin}
+          >
+            GitHubでログイン
+          </Button>
+        </Stack>
+        <Center
+          w={300}
+          h={300}
+          bg="gray.2"
+          style={{ borderRadius: "var(--mantine-radius-lg)" }}
         >
-          GitHubでログイン
-        </Button>
-      </Stack>
+          <IconLogin2
+            size="80%"
+            stroke={1.4}
+            color="var(--mantine-color-red-7)"
+          />
+        </Center>
+      </Flex>
     </AppModal>
   );
 };
