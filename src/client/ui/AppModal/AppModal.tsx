20fc2fa2ecbbdc16fa5a6bf6769d823b8ef17f4d
hwld <0701hw@gmail.com>
2023-11-08 11:59:10 +0900

    feat: AppModalのUIを変更

--- src/client/ui/AppModal/AppModal.tsx
+++ src/client/ui/AppModal/AppModal.tsx
@@ -10,7 +10,16 @@ export const AppModal: React.FC<ModalProps> = ({ children, ...props }) => {
     <Box style={{ display: "none" }} onClick={stopPropagation}>
       <Modal
         centered
-        transitionProps={{ exitDuration: 150 }}
+        transitionProps={{
+          transition: {
+            in: { opacity: 1, transform: "translateY(0px)" },
+            out: { opacity: 0, transform: "translateY(-30px)" },
+            transitionProperty: "opacity,transform",
+          },
+          timingFunction: "ease-in-out",
+          duration: 250,
+          exitDuration: 200,
+        }}
         overlayProps={{ backgroundOpacity: 0.4 }}
         radius="lg"
         classNames={{
