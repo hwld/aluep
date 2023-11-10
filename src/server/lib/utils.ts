7365364048686a4e6a12a9dcafe15be21625809c
hwld <0701hw@gmail.com>
2023-11-11 08:30:08 +0900

    chore: 依存関係の整理

--- src/server/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 指定されたミリ秒待機する
- * デバッグ用
- * @param ms
- */
-export const __sleep = async (ms: number) => {
-  console.error("sleepが使用されています");
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
