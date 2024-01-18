6a5c2ab5add1f63e4041afdf8788a55374dcd774
hwld <0701hw@gmail.com>
2024-01-18 14:29:07 +0900

    chore: vercelはタイムアウトがキツイのでCloud Runに戻す

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 let nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: "standalone",
   // ブラウザバックしたときに以前のスクロール位置を保持する
   // スクロールにwindow.scrollを使用しているっぽいので、例えば#__nextに
   // height:100dvh, overflow:autoとかを設定しても動かない。
